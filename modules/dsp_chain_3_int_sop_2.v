0bac42e42d8ad4577a26de1adc1fbddc62f6203f
Your Name <karan@europa1.ece.utexas.edu>
2022-04-08 14:39:23 -0500

    Adding dsp chain modules

---
+++ modules/dsp_chain_3_int_sop_2.v
@@ -0,0 +1,22 @@
+module dsp_chain_3_int_sop_2 (clk,reset,ax1,ay1,bx1,by1,ax2,ay2,bx2,by2,ax3,ay3,bx3,by3,result);
+
+input clk; 
+input reset; 
+input [17:0] ax1, bx1, ax2, bx2, ax3, bx3; 
+input [18:0] ay1, by1, ay2, by2, ay3, by3; 
+output reg [36:0] result; 
+
+wire [36:0] chainout0, chainout1, chainout2; 
+wire [36:0] chainin0, chainin1, chainin2; 
+wire [36:0] resulta1, resulta2, resulta3;
+
+assign chainin0 = 37'd0;
+
+int_sop_2 inst1 (.clk(clk),.reset(reset),.ax(ax1),.bx(bx1),.ay(ay1),.by(by1),.mode_sigs(11'd0),.chainin(chainin0),.resulta(resulta1),.chainout(chainout0));
+int_sop_2 inst2 (.clk(clk),.reset(reset),.ax(ax2),.bx(bx2),.ay(ay2),.by(by2),.mode_sigs(11'd0),.chainin(chainout0),.resulta(resulta2),.chainout(chainout1));
+int_sop_2 inst3 (.clk(clk),.reset(reset),.ax(ax3),.bx(bx3),.ay(ay3),.by(by3),.mode_sigs(11'd0),.chainin(chainout1),.resulta(resulta3),.chainout(chainout2));
+
+assign result = resulta3;
+
+
+endmodule 
---
+++ modules/dsp_chain_4_fp16_sop2_mult.v
@@ -0,0 +1,27 @@
+module dsp_chain_4_fp16_sop2_mult (clk,reset,top_a1,top_b1,bot_a1,bot_b1,top_a2,top_b2,bot_a2,bot_b2,top_a3,top_b3,bot_a3,bot_b3,top_a4,top_b4,bot_a4,bot_b4,result); 
+
+input clk;
+input reset; 
+input [15:0] top_a1, top_b1, bot_a1, bot_b1, top_a2, top_b2, bot_a2, bot_b2, top_a3, top_b3, bot_a3, bot_b3, top_a4, top_b4, bot_a4, bot_b4; 
+output reg [31:0] result; 
+
+wire [31:0] chainin0, chainin1, chainin2, chainin3;
+wire [31:0] chainout0, chainout1, chainout2, chainout3; 
+wire [31:0] fp32_in1, fp32_in2, fp32_in3, fp32_in4; 
+wire [31:0] result1, result2, result3, result4; 
+
+assign fp32_in1 = 32'd0; 
+assign fp32_in2 = 32'd0;
+assign fp32_in3 = 32'd0;
+assign fp32_in4 = 32'd0;
+assign chainin0 = 32'd0; 
+
+fp16_sop2_mult inst1 (.clk(clk),.reset(reset),.top_a(top_a1),.top_b(top_b1),bot_a(bot_a1),.bot_b(bot_b1),.fp32_in(fp32_in1),.mode_sigs(11'd0),.chainin(chainin0),.chainout(chainout0),.result(result1)); 
+fp16_sop2_mult inst1 (.clk(clk),.reset(reset),.top_a(top_a2),.top_b(top_b2),bot_a(bot_a2),.bot_b(bot_b2),.fp32_in(fp32_in2),.mode_sigs(11'd0),.chainin(chainout0),.chainout(chainout1),.result(result2)); 
+fp16_sop2_mult inst1 (.clk(clk),.reset(reset),.top_a(top_a3),.top_b(top_b3),bot_a(bot_a3),.bot_b(bot_b3),.fp32_in(fp32_in3),.mode_sigs(11'd0),.chainin(chainout1),.chainout(chainout2),.result(result3)); 
+fp16_sop2_mult inst1 (.clk(clk),.reset(reset),.top_a(top_a4),.top_b(top_b4),bot_a(bot_a4),.bot_b(bot_b4),.fp32_in(fp32_in4),.mode_sigs(11'd0),.chainin(chainout2),.chainout(chainout3),.result(result4)); 
+
+assign result = result4;  
+
+
+endmodule
