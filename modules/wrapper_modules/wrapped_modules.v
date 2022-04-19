f15d89d435e72a9ff7a7a9f062b1aa9eb719c789
Your Name <karan@europa1.ece.utexas.edu>
2022-04-19 09:47:09 -0500

    updates to wrapper module

--- modules/wrapper_modules/wrapped_modules.v
+++ modules/wrapper_modules/wrapped_modules.v
@@ -46,3 +46,44 @@ sigmoid inst (.x(inp),.sig_out(outp));
 
 endmodule
 
+module systolic_array_4_16bit (input clk, input reset, input [275:0] inp, output reg [97:0] outp);
+
+matmul_4x4_systolic inst(
+ .clk(clk),
+ .reset(reset),
+ .pe_reset(inp[0]),
+ .start_mat_mul(inp[1]),
+ .done_mat_mul(outp[0]),
+ .address_mat_a(inp[11:1]),
+ .address_mat_b(inp[22:12]),
+ .address_mat_c(inp[33:23]),
+ .address_stride_a(inp[40:33]),
+ .address_stride_b(inp[48:41]),
+ .address_stride_c(inp[56:49]),
+ .a_data(inp[88:57]),
+ .b_data(inp[120:89]),
+ .a_data_in(inp[152:121]), //Data values coming in from previous matmul - systolic connections
+ .b_data_in(inp[184:153]),
+ .c_data_in(inp[216:185]), //Data values coming in from previous matmul - systolic shifting
+ .c_data_out(outp[32:1]), //Data values going out to next matmul - systolic shifting
+ .a_data_out(outp[64:33]),
+ .b_data_out(outp[96:65]),
+ .a_addr(inp[227:217]),
+ .b_addr(inp[238:228]),
+ .c_addr(inp[249:239]),
+ .c_data_available(outp[97]),
+ .validity_mask_a_rows(inp[252:250]),
+ .validity_mask_a_cols_b_rows(inp[255:253]),
+ .validity_mask_b_cols(inp[258:256]),
+ .final_mat_mul_size(inp[266:259]),
+ .a_loc(inp[267:260]),
+ .b_loc(inp[275:268])
+);
+
+endmodule
+
+module dsp_chain_2_int_sop_2_module (input clk, input reset, input [275:0] inp, output reg [36:0] outp); 
+
+dsp_chain_2_int_sop_2 inst(.clk(clk),.reset(reset),.ax1(inp[17:0]),.ay1(inp[36:18]),.bx1(inp[54:37]),.by1(inp[73:55]),.ax2(inp[91:74]),.ay2(inp[110:92]),.bx2(inp[128:111]),.by2(inp[147:129]),.result(outp[36:0]));
+
+endmodule
