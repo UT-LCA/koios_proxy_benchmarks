269a6ce4b7789f33fc966ebc2056a1a5f427acee
Your Name <karan@europa1.ece.utexas.edu>
2022-05-07 07:04:06 -0500

    adding fifo modules

---
+++ modules/fifo_256_40bit.v
@@ -0,0 +1,158 @@
+module fifo_256_40bit
+	#(parameter dw=40,
+	  parameter aw=8)
+	(clk, rst, clr, din, we, dout, re,
+	full, empty);
+
+parameter max_size = 1<<aw;
+
+input 			clk, rst, clr;
+input 	[dw-1:0]	din;
+input 			we;
+output	[dw-1:0]	dout;
+input 	wire		re;
+output			full;
+output			empty;
+
+////////////////////////////////////////////////////////////////////
+//
+// Local Wires
+//
+wire 	[dw-1:0]	din_nc;
+wire 	[dw-1:0]	out_nc;
+reg		[aw-1:0]	wp;
+wire	[aw-1:0]	wp_pl1;
+reg		[aw-1:0]	rp;
+wire	[aw-1:0]	rp_pl1;
+reg					gb;
+
+////////////////////////////////////////////////////////////////////
+//
+// Memory Block
+//
+dpram_256_40bit #(
+	.AWIDTH(aw),
+	.DWIDTH(dw)
+	) u0 (
+	.clk(clk),
+	.address_a(rp),
+	.wren_a(0),
+	.data_a(din_nc),
+	.out_a(dout),
+	.address_b(wp),
+	.wren_b(we),
+	.data_b(din),
+	.out_b(out_nc)
+);
+
+
+////////////////////////////////////////////////////////////////////
+//
+// Misc Logic
+//
+
+always @(posedge clk or posedge rst)
+	if(rst)	wp <= {aw{1'b0}};
+	else
+	if(clr)		wp <= {aw{1'b0}};
+	else
+	if(we)		wp <= wp_pl1;
+
+assign wp_pl1 = wp + { {aw-1{1'b0}}, 1'b1};
+
+always @(posedge clk or posedge rst)
+	if(rst)		rp <= {aw{1'b0}};
+	else
+	if(clr)		rp <= {aw{1'b0}};
+	else
+	if(re)		rp <= rp_pl1;
+
+assign rp_pl1 = rp + { {aw-1{1'b0}}, 1'b1};
+
+////////////////////////////////////////////////////////////////////
+//
+// Combinatorial Full & Empty Flags
+//
+
+assign empty = ((wp == rp) & !gb);
+assign full  = ((wp == rp) &  gb);
+
+// Guard Bit ...
+always @(posedge clk or posedge rst)
+	if(rst)						gb <= 1'b0;
+	else
+	if(clr)						gb <= 1'b0;
+	else
+	if((wp_pl1 == rp) & we)		gb <= 1'b1;
+	else
+	if(re)						gb <= 1'b0;
+
+endmodule
+
+module dpram_256_40bit #(
+	parameter DWIDTH = 40,
+	parameter AWIDTH = 8
+  )
+  (
+	clk,
+	address_a,
+	address_b,
+	wren_a,
+	wren_b,
+	data_a,
+	data_b,
+	out_a,
+	out_b
+);
+
+parameter NUM_WORDS=1<<AWIDTH;
+
+input clk;
+input [(AWIDTH-1):0] address_a;
+input [(AWIDTH-1):0] address_b;
+input  wren_a;
+input  wren_b;
+input [(DWIDTH-1):0] data_a;
+input [(DWIDTH-1):0] data_b;
+output reg [(DWIDTH-1):0] out_a;
+output reg [(DWIDTH-1):0] out_b;
+
+`ifdef SIMULATION
+
+	reg [DWIDTH-1:0] ram[NUM_WORDS-1:0];
+
+	always @ (posedge clk) begin
+		if (wren_a) begin
+			ram[address_a] <= data_a;
+		end
+		else begin
+			out_a <= ram[address_a];
+		end
+	end
+	  
+	always @ (posedge clk) begin 
+		if (wren_b) begin
+			ram[address_b] <= data_b;
+		end 
+		else begin
+			out_b <= ram[address_b];
+		end
+	end
+
+`else
+
+	dual_port_ram u_dual_port_ram(
+	.addr1(address_a),
+	.we1(wren_a),
+	.data1(data_a),
+	.out1(out_a),
+	.addr2(address_b),
+	.we2(wren_b),
+	.data2(data_b),
+	.out2(out_b),
+	.clk(clk)
+	);
+
+`endif
+
+endmodule
