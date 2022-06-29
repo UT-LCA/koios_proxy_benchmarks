8ec55fa851f0939d87ee4d8945daa259f3545742
Your Name <karan@europa1.ece.utexas.edu>
2022-06-29 17:48:19 -0500

    improving frequency by pipelining

--- modules/tensor_block_bf16.v
+++ modules/tensor_block_bf16.v
@@ -378,105 +378,115 @@ module FPAddSub_single_32(
 		flags
 	);
 
-// Clock and reset
-	input clk ;										// Clock signal
-	input rst ;										// Reset (active high, resets pipeline registers)
-	
-	// Input ports
-	input [31:0] a ;								// Input A, a 32-bit floating point number
-	input [31:0] b ;								// Input B, a 32-bit floating point number
-	input operation ;								// Operation select signal
-	
-	// Output ports
-	output [31:0] result ;						// Result of the operation
-	output [4:0] flags ;							// Flags indicating exceptions according to IEEE754
-
-	wire [68:0]pipe_1;
-	wire [54:0]pipe_2;
-	wire [45:0]pipe_3;
-
-
-//internal module wires
-
-//output ports
-	wire Opout;
-	wire Sa;
-	wire Sb;
-	wire MaxAB;
-	wire [7:0] CExp;
-	wire [4:0] Shift;
-	wire [22:0] Mmax;
-	wire [4:0] InputExc;
-	wire [23:0] Mmin_3;
-
-	wire [32:0] SumS_5 ;
-	wire [4:0] Shift_1;							
-	wire PSgn ;							
-	wire Opr ;	
-	
-	wire [22:0] NormM ;				// Normalized mantissa
-	wire [8:0] NormE ;					// Adjusted exponent
-	wire ZeroSum ;						// Zero flag
-	wire NegE ;							// Flag indicating negative exponent
-	wire R ;								// Round bit
-	wire S ;								// Final sticky bit
-	wire FG ;
-
-FPAddSub_a_32 M1(a,b,operation,Opout,Sa,Sb,MaxAB,CExp,Shift,Mmax,InputExc,Mmin_3);
-
-FpAddSub_b_32 M2(pipe_1[51:29],pipe_1[23:0],pipe_1[67],pipe_1[66],pipe_1[65],pipe_1[68],SumS_5,Shift_1,PSgn,Opr);
-
-FPAddSub_c_32 M3(pipe_2[54:22],pipe_2[21:17],pipe_2[16:9],NormM,NormE,ZeroSum,NegE,R,S,FG);
-
-FPAddSub_d_32 M4(pipe_3[13],pipe_3[22:14],pipe_3[45:23],pipe_3[11],pipe_3[10],pipe_3[9],pipe_3[8],pipe_3[7],pipe_3[6],pipe_3[5],pipe_3[12],pipe_3[4:0],result,flags );
-
-
-/*
-pipe_1:
-	[68] Opout;
-	[67] Sa;
-	[66] Sb;
-	[65] MaxAB;
-	[64:57] CExp;
-	[56:52] Shift;
-	[51:29] Mmax;
-	[28:24] InputExc;
-	[23:0] Mmin_3;	
-*/
-
-assign pipe_1 = {Opout,Sa,Sb,MaxAB,CExp,Shift,Mmax,InputExc,Mmin_3};
-
-/*
-pipe_2:
-	[54:22]SumS_5;
-	[21:17]Shift;
-	[16:9]CExp;	
-	[8]Sa;
-	[7]Sb;
-	[6]operation;
-	[5]MaxAB;	
-	[4:0]InputExc
-*/
-
-assign pipe_2 = {SumS_5,Shift_1,pipe_1[64:57], pipe_1[67], pipe_1[66], pipe_1[68], pipe_1[65], pipe_1[28:24] };
-
-/*
-pipe_3:
-	[45:23] NormM ;				
-	[22:14] NormE ;					
-	[13]ZeroSum ;						
-	[12]NegE ;							
-	[11]R ;								
-	[10]S ;								
-	[9]FG ;
-	[8]Sa;
-	[7]Sb;
-	[6]operation;
-	[5]MaxAB;	
-	[4:0]InputExc
-*/
-
-assign pipe_3 = {NormM,NormE,ZeroSum,NegE,R,S,FG, pipe_2[8], pipe_2[7], pipe_2[6], pipe_2[5], pipe_2[4:0] };
+  // Clock and reset
+  	input clk ;										// Clock signal
+  	input rst ;										// Reset (active high, resets pipeline registers)
+  	
+  	// Input ports
+  	input [31:0] a ;								// Input A, a 32-bit floating point number
+  	input [31:0] b ;								// Input B, a 32-bit floating point number
+  	input operation ;								// Operation select signal
+  	
+  	// Output ports
+  	output [31:0] result ;						// Result of the operation
+  	output [4:0] flags ;							// Flags indicating exceptions according to IEEE754
+  
+  	reg [68:0]pipe_1;
+  	reg [54:0]pipe_2;
+  	reg [45:0]pipe_3;
+  
+  
+  //internal module wires
+  
+  //output ports
+  	wire Opout;
+  	wire Sa;
+  	wire Sb;
+  	wire MaxAB;
+  	wire [7:0] CExp;
+  	wire [4:0] Shift;
+  	wire [22:0] Mmax;
+  	wire [4:0] InputExc;
+  	wire [23:0] Mmin_3;
+  
+  	wire [32:0] SumS_5 ;
+  	wire [4:0] Shift_1;							
+  	wire PSgn ;							
+  	wire Opr ;	
+  	
+  	wire [22:0] NormM ;				// Normalized mantissa
+  	wire [8:0] NormE ;					// Adjusted exponent
+  	wire ZeroSum ;						// Zero flag
+  	wire NegE ;							// Flag indicating negative exponent
+  	wire R ;								// Round bit
+  	wire S ;								// Final sticky bit
+  	wire FG ;
+  
+  FPAddSub_a_32 M1(a,b,operation,Opout,Sa,Sb,MaxAB,CExp,Shift,Mmax,InputExc,Mmin_3);
+  
+  FpAddSub_b_32 M2(pipe_1[51:29],pipe_1[23:0],pipe_1[67],pipe_1[66],pipe_1[65],pipe_1[68],SumS_5,Shift_1,PSgn,Opr);
+  
+  FPAddSub_c_32 M3(pipe_2[54:22],pipe_2[21:17],pipe_2[16:9],NormM,NormE,ZeroSum,NegE,R,S,FG);
+  
+  FPAddSub_d_32 M4(pipe_3[13],pipe_3[22:14],pipe_3[45:23],pipe_3[11],pipe_3[10],pipe_3[9],pipe_3[8],pipe_3[7],pipe_3[6],pipe_3[5],pipe_3[12],pipe_3[4:0],result,flags );
+  
+  
+  always @ (posedge clk) begin	
+  		if(rst) begin
+  			pipe_1 <= 0;
+  			pipe_2 <= 0;
+  			pipe_3 <= 0;
+  		end 
+  		else begin
+  /*
+  pipe_1:
+  	[68] Opout;
+  	[67] Sa;
+  	[66] Sb;
+  	[65] MaxAB;
+  	[64:57] CExp;
+  	[56:52] Shift;
+  	[51:29] Mmax;
+  	[28:24] InputExc;
+  	[23:0] Mmin_3;	
+  */
+  
+  pipe_1 <= {Opout,Sa,Sb,MaxAB,CExp,Shift,Mmax,InputExc,Mmin_3};
+  
+  /*
+  pipe_2:
+  	[54:22]SumS_5;
+  	[21:17]Shift;
+  	[16:9]CExp;	
+  	[8]Sa;
+  	[7]Sb;
+  	[6]operation;
+  	[5]MaxAB;	
+  	[4:0]InputExc
+  */
+  
+  pipe_2 <= {SumS_5,Shift_1,pipe_1[64:57], pipe_1[67], pipe_1[66], pipe_1[68], pipe_1[65], pipe_1[28:24] };
+  
+  /*
+  pipe_3:
+  	[45:23] NormM ;				
+  	[22:14] NormE ;					
+  	[13]ZeroSum ;						
+  	[12]NegE ;							
+  	[11]R ;								
+  	[10]S ;								
+  	[9]FG ;
+  	[8]Sa;
+  	[7]Sb;
+  	[6]operation;
+  	[5]MaxAB;	
+  	[4:0]InputExc
+  */
+  
+  pipe_3 <= {NormM,NormE,ZeroSum,NegE,R,S,FG, pipe_2[8], pipe_2[7], pipe_2[6], pipe_2[5], pipe_2[4:0] };
+  
+  end
+  end
 
 
 endmodule
